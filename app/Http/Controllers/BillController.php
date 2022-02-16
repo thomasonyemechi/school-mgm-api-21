83432464a523558c9beae0b1dbb497678012eadf
Thomas Onyemechi <thomasonyemechi03@gmail.com>
2022-02-16 16:18:51 +0100

    bb

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Validator;
 
 class BillController extends Controller
 {
+    // id type amount 
 
     function getStartAndEndDate($week, $year) {
         $dto = new DateTime();
